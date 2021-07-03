622378111a6416e5467eb35885ca424a91d3a8f7
Valeriya Kistrina <vkistrin@sfu.ca>
2021-07-03 16:42:31 -0700

    Yoga Youtube video feature - VK

--- app/src/main/java/ca/deedhacks/TiredActivity.java
+++ app/src/main/java/ca/deedhacks/TiredActivity.java
@@ -2,18 +2,32 @@ package ca.deedhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListAdapter;
 
-public class TiredActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class TiredActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.getSupportActionBar().hide();
         setContentView(R.layout.activity_tired);
+
+        Button yogaBtn = findViewById(R.id.yoga_btn_tasks);
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TiredActivity.this, YogaActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
