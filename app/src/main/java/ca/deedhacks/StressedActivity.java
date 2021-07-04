47d71f31c607e28580572a3775e591b219f9a178
yca321 <yimingcainy@gmail.com>
2021-07-04 07:02:41 -0700

    one music added for stressed activity

--- app/src/main/java/ca/deedhacks/StressedActivity.java
+++ app/src/main/java/ca/deedhacks/StressedActivity.java
@@ -2,13 +2,50 @@ package ca.deedhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.view.View;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class StressedActivity extends AppCompatActivity {
+
+    MediaPlayer music;
+    TextView SongName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stressed);
+
+        music = MediaPlayer.create(
+                this, R.raw.sound1);
+
+
     }
+
+    public void musicplay(View v)
+    {
+        music.start();
+    }
+
+    // Pausing the music
+    public void musicpause(View v)
+    {
+        music.pause();
+    }
+
+    // Stoping the music
+    public void musicstop(View v)
+    {
+        music.stop();
+        music = MediaPlayer.create(
+                this, R.raw.sound1);
+    }
+
+
+
+
 }
