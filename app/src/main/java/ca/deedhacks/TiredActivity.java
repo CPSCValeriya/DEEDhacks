b154dd81f25c045e1ac9861e449f5202470063ef
yca321 <yimingcainy@gmail.com>
2021-07-03 19:06:33 -0700

    Revert "walk activity layout without function."
    This reverts commit 874afc3d5502e6801425e9f0849f53ca535b849d.

--- app/src/main/java/ca/deedhacks/TiredActivity.java
+++ app/src/main/java/ca/deedhacks/TiredActivity.java
@@ -2,12 +2,10 @@ package ca.deedhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListAdapter;
 
 public class TiredActivity extends AppCompatActivity {
@@ -16,17 +14,6 @@ public class TiredActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tired);
-
-        Button walkBtn = findViewById(R.id.btn_walk);
-        walkBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TiredActivity.this, WalkActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
 
     }
 
