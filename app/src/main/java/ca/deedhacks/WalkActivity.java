6de92e34c7a039b2207372f298369265932b0e19
yca321 <yimingcainy@gmail.com>
2021-07-03 19:06:30 -0700

    Revert "walk activity with timer working properly"
    This reverts commit 6000b94098b1ff784170ac84f5d5673434fad869.

--- app/src/main/java/ca/deedhacks/WalkActivity.java
+++ app/src/main/java/ca/deedhacks/WalkActivity.java
@@ -1,7 +1,6 @@
 package ca.deedhacks;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.os.Bundle;
@@ -9,119 +8,64 @@ import android.os.Bundle;
 import android.widget.Chronometer;
 import android.widget.Button;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class WalkActivity extends AppCompatActivity{
 
-    TextView timerText;
-    Button stopStartButton;
-
-    boolean ifTimer = false;// if timer started
-
-    Timer timer;
-    TimerTask timerTask;
-    Double time = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
 
-        timerText = (TextView) findViewById(R.id.text_timer);
-        stopStartButton = (Button) findViewById(R.id.btn_start);
 
-        timer = new Timer();
+
+
+    }
+    /* implements View.OnClickListener,Chronometer.OnChronometerTickListener
+    private Chronometer chronometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        Button btn_start = (Button) findViewById(R.id.starter);
+        Button btn_stop = (Button) findViewById(R.id.stopper);
+
+
+        chronometer.setOnChronometerTickListener(this);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
 
     }
 
-    public void resetTapped(View view){
-
-        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
-        resetAlert.setTitle("Reset");
-        resetAlert.setMessage("Reset The Timer?");
-        resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-                if(timerTask != null){
-                    timerTask.cancel();
-                    time = 0.0;
-                    ifTimer = false;
-                    timerText.setText(formatTime(0,0,0));
-                }
-
-            }
-        });
-
-        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //cancel reset
-            }
-        });
-
-        resetAlert.show();
-
-    }
-
-    public void startStopTapped(View view){
-
-        if(ifTimer == false){
-           ifTimer = true;
-           stopStartButton.setText("STOP");
-
-           startTimer();
-        }
-        else{
-            ifTimer = false;
-            stopStartButton.setText("START");
-
-            timerTask.cancel();
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.starter:
+                chronometer.start();// timer start
+                break;
+            case R.id.stopper:
+                chronometer.stop();// stop timer
+                break;
         }
     }
 
-    private void startTimer() {
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
 
-        timerTask = new TimerTask() {
-            @Override
-            public void run() {
+        String time = chronometer.getText().toString();
+        if (time.equals("00:00")) {
+            Toast.makeText(WalkActivity.this, "times up", Toast.LENGTH_SHORT).show();
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        time++;
-                        timerText.setText(getTimerText());
-                    }
-                });
-
-                
-            }
-        };
-        timer.scheduleAtFixedRate(timerTask, 0, 1000);
-    }
-
-    private String getTimerText() {
-        
-        int rounded = (int)Math.round(time);
-       
-        int sec = ((rounded % 86400) % 3600) % 60;
-        int min = ((rounded % 86400) % 3600) / 60;
-        int hr = ((rounded % 86400) / 3600);
-        
-        return formatTime(sec, min, hr);
-    }
-
-    private String formatTime(int sec, int min, int hr) {
-
-        return String.format("%02d", hr) + ":" + String.format("%02d", min) + ":" + String.format("%02d", sec);
-    }
-
+        }
+    }*/
 }
